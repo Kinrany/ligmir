2b85679d0e9728583720db8eb8a22624f88cc7d1
Ruslan Fadeev <kinrany@yandex.com>
2021-01-16 21:12:28 +0300

    Reformat

--- src/main.rs
+++ src/main.rs
@@ -20,42 +20,45 @@ fn download_skill_modifiers(url: &str) -> Fallible<Vec<(String, i32)>> {
 	})?;
 
 	// Wait for network/javascript/dom to make the skill list available
-	let element = tab
-		.wait_for_element_with_custom_timeout("div.ct-skills", std::time::Duration::from_secs(10))?;
+	let element = tab.wait_for_element_with_custom_timeout(
+		"div.ct-skills",
+		std::time::Duration::from_secs(10),
+	)?;
 
 	// Parse the skill list
 	let skills = element
-      .call_js_fn(
-        r#"
-        function() {
-          const items = this.querySelectorAll(".ct-skills__item");
-          const skillValues = [...items].map(item => {
-            const skill = item.querySelector(".ct-skills__col--skill");
-            const modifier = item.querySelector(".ct-skills__col--modifier");
-            return [skill, modifier];
-          });
-          const text = skillValues
-            .map(([skill, modifier]) => `${skill.innerText},${modifier.innerText.replace("\n", "")}`)
-            .join(";");
-          return text;
-        }"#,
-        true,
-      )?
-      .value
-      .ok_or(err_msg("Function did not return a value"))?
-      .to_string()
-      .replace("\"", "")
-      .split(";")
-      .map(
-        |s| match s.split(",").take(2).collect::<Vec<&str>>().as_slice() {
-          [a, b, ..] => Ok(((*a).to_owned(), b.parse::<i32>()?)),
-          _ => {
-            let message = format!("Cannot parse string \"{}\" into skill name and modifier", s);
-            Err(err_msg(message))
-          }
-        },
-      )
-      .collect::<Fallible<Vec<(String, i32)>>>()?;
+		.call_js_fn(
+			r#"
+				function() {
+					const items = this.querySelectorAll(".ct-skills__item");
+					const skillValues = [...items].map(item => {
+						const skill = item.querySelector(".ct-skills__col--skill");
+						const modifier = item.querySelector(".ct-skills__col--modifier");
+						return [skill, modifier];
+					});
+					const text = skillValues
+						.map(([skill, modifier]) => `${skill.innerText},${modifier.innerText.replace("\n", "")}`)
+						.join(";");
+					return text;
+				}"#,
+			true,
+		)?
+		.value
+		.ok_or(err_msg("Function did not return a value"))?
+		.to_string()
+		.replace("\"", "")
+		.split(";")
+		.map(
+			|s| match s.split(",").take(2).collect::<Vec<&str>>().as_slice() {
+				[a, b, ..] => Ok(((*a).to_owned(), b.parse::<i32>()?)),
+				_ => {
+					let message =
+						format!("Cannot parse string \"{}\" into skill name and modifier", s);
+					Err(err_msg(message))
+				}
+			},
+		)
+		.collect::<Fallible<Vec<(String, i32)>>>()?;
 
 	Ok(skills)
 }
@@ -79,7 +82,8 @@ async fn handle_update(token: String, update: Update) {
 
 	let result = tokio::task::spawn_blocking(|| {
 		println!("Beginning to download");
-		let skills = download_skill_modifiers("https://www.dndbeyond.com/characters/27570282/JhoG2D");
+		let skills =
+			download_skill_modifiers("https://www.dndbeyond.com/characters/27570282/JhoG2D");
 		println!("Finished downloading");
 		skills
 	})
